2079889c11128ea99496785c2b83ff50c6a7a8db
Kevin <Kevin@Kevin-Tablet>
2015-02-16 15:22:22 -0500

    Changing message to test merging w conflict

--- src/TestDriver.java
+++ src/TestDriver.java
@@ -4,7 +4,7 @@ public class TestDriver {
 	//Fenton Test 3
 	public static void main(String[] args) {
 		// Dustin Thompson
-		System.out.println("Hello World");
+		System.out.println("Hello Kevin");
 	}
 	//zach
 
