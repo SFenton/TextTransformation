f73955183215070841e8281b7779fb99bb3e1919
hzach3 <hzach3@gmail.com>
2015-02-27 15:25:20 -0500

    Created citation class.

---
+++ src/Citation.java
@@ -0,0 +1,21 @@
+
+/*
+ * This class represents a citation in the document being read from.
+ * Contains a default set of fields that are represented by strings.
+ */
+public class Citation {
+	public String Authors, Date1, Title, Publication, Location, Date2, Volume,
+			Issue, Pages;
+
+	public Citation() {
+		Authors = "";
+		Date1 = "";
+		Title = "";
+		Publication = "";
+		Location = "";
+		Date2 = "";
+		Volume = "";
+		Issue = "";
+		Pages = "";
+	}
+}
